7ccb535794db716f0e288e2e2628dab0977b4d2c
zipgalrae <swseo@kric.com>
2023-03-16 14:50:35 +0900

    Router 분리

--- frontend/src/scripts/router.js
+++ frontend/src/scripts/router.js
@@ -2,6 +2,8 @@ import Home from '@/pages/Home'
 import Login from '@/pages/Login'
 import { createRouter, createWebHistory } from 'vue-router'
 
+// Router은 따로 분리해서 사용
+
 const routes = [
     { path: '/', component: Home },
     { path: '/login', component: Login }
